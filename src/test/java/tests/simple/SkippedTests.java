408e70c6d8a060dc2a55b07f04869e1d97b86e6b
Mikhail Solovyov <solovev02@yandex.ru>
2025-11-01 17:38:35 +0300

    added  extends TestBase to simpletests

--- src/test/java/tests/simple/SkippedTests.java
+++ src/test/java/tests/simple/SkippedTests.java
@@ -3,11 +3,12 @@ package tests.simple;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import tests.TestBase;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Tag("simple")
-public class SkippedTests {
+public class SkippedTests extends TestBase {
 
     @Test
     @Disabled
