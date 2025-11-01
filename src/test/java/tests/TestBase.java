f5db4181af9ec845159c64996810e2c87cc1faa7
Mikhail Solovyov <solovev02@yandex.ru>
2025-11-01 18:10:19 +0300

    MC

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -54,6 +54,6 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        Attach.getVideoUrl();
+        Attach.addVideo();
     }
 }
